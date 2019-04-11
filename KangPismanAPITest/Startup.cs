9504c991f66378fee68fd3f07ad0cfb15367d2d6
yogichandras <yogey264@gmail.com>
2019-04-11 14:17:56 +0700

    add cors

--- KangPismanAPITest/Startup.cs
+++ KangPismanAPITest/Startup.cs
@@ -24,6 +24,10 @@ namespace KangPismanAPITest
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddCors(c =>
+            {
+                c.AddPolicy("AllowOrigin",options => options.AllowAnyOrigin());
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
